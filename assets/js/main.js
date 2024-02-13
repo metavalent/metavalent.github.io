ed0129c46245eab042b85fbe1f3b2bf609ac6783
metavalent stigmergy <15897937+metavalent@users.noreply.github.com>
2024-02-13 09:42:19 -0700

    Issue #1 rollback some changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 var sectionHeight = function() {
-    var total    = $.find(window).height(),
-        $section = $.find('section').css('height','auto');
+    var total    = $(window).height(),
+        $section = $('section').css('height','auto');
   
     if ($section.outerHeight(true) < total) {
       var margin = $section.outerHeight(true) - $section.height();
@@ -10,24 +10,24 @@ var sectionHeight = function() {
     }
   }
   
-  $.find(window).resize(sectionHeight);
+  $(window).resize(sectionHeight);
   
-  $.find(function() {
-    $.find("section h1, section h2, section h3").each(function(){
-      $.find("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $.find(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $.find(this).text() + "</a></li>");
-      $.find(this).attr("id",$.find(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-      $.find("nav ul li:first-child a").parent().addClass("active");
+  $(function() {
+    $("section h1, section h2, section h3").each(function(){
+      $.find("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+      $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+      $("nav ul li:first-child a").parent().addClass("active");
     });
   
-    $.find("nav ul li").on("click", "a", function(event) {
-      var position = $.find($.find(this).attr("href")).offset().top - 190;
-      $.find("html, body").animate({scrollTop: position}, 400);
-      $.find("nav ul li a").parent().removeClass("active");
-      $.find(this).parent().addClass("active");
+    $("nav ul li").on("click", "a", function(event) {
+      var position = $($(this).attr("href")).offset().top - 190;
+      $("html, body").animate({scrollTop: position}, 400);
+      $("nav ul li a").parent().removeClass("active");
+      $(this).parent().addClass("active");
       event.preventDefault();
     });
   
     sectionHeight();
   
-    $.find('img').on('load', sectionHeight);
+    $('img').on('load', sectionHeight);
   });
